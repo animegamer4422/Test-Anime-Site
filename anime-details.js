cee3afcbdb4e2cea1a7b0f41f262e8b7bc8357e3
animegamer4422 <animegamer4422@gmail.com>
2023-05-13 15:45:40 +0530

    hopefully this works

--- anime-details.js
+++ anime-details.js
@@ -27,38 +27,62 @@ async function displayDetails(anime) {
   const image = document.getElementById('anime-image');
   const description = document.getElementById('anime-description');
   const episodes = document.getElementById('anime-episodes');
-  const href = document.getElementById('anime-url');
 
   title.textContent = anime.title;
   image.src = anime.image;
   image.alt = anime.title;
   description.textContent = anime.description;
 
-// Display the list of episodes
-for (const episode of anime.episodes) {
-  const listItem = document.createElement('li');
-  const link = document.createElement('a');
-  link.textContent = `Episode ${episode.number}`;
+  // Display the list of episodes
+  for (const episode of anime.episodes) {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.textContent = `Episode ${episode.number}`;
 
-  // Add data attributes for episode number and any other required parameters
-  link.dataset.episodeNumber = episode.number;
-  link.dataset.episodeId = episode.id;
+    // Add data attributes for episode number and any other required parameters
+    link.dataset.episodeNumber = episode.number;
+    link.dataset.episodeId = episode.id;
 
-  link.classList.add('episode');
+    link.classList.add('episode');
 
-  listItem.appendChild(link);
-  episodes.appendChild(listItem);
-}
+    listItem.appendChild(link);
+    episodes.appendChild(listItem);
+  }
 
-// Add the click event listener to the dynamically created episode elements
-document.querySelectorAll('.episode').forEach(episodeLink => {
-  episodeLink.addEventListener('click', async (event) => {
-    const episodeNumber = event.target.dataset.episodeNumber;
-    const episodeId = event.target.dataset.episodeId;
+  // Add the click event listener to the dynamically created episode elements
+  document.querySelectorAll('.episode').forEach(episodeLink => {
+    episodeLink.addEventListener('click', async (event) => {
+      const episodeNumber = event.target.dataset.episodeNumber;
+      const episodeId = event.target.dataset.episodeId;
+      
 
-    // Now navigate to the video-player.html page and pass the necessary parameters
-    window.location.href = `video-player.html?episodeNumber=${episodeNumber}&episodeId=${episodeId}`;
+      // Now navigate to the video-player.html page and pass the necessary parameters
+      window.location.href = `video-player.html?episodeNumber=${episodeNumber}&episodeId=${episodeId}`;
+    });
   });
-});
-
 }
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const animeId = urlParams.get('animeId');
+
+  if (animeId) {
+    const apiUrl = `https://api.consumet.org/anime/gogoanime/info/${animeId}`;
+
+    try {
+      const response = await fetch(apiUrl);
+
+      if (response.ok) {
+        const data = await response.json();
+        await displayDetails(data);
+      } else {
+        console.error('Error fetching data:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  } else {
+    // Redirect to the main page if there's no animeId in the URL parameters
+    window.location.href = 'index.html';
+  }
+});
